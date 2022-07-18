5f790cdc4e98ef73eaaa1098639d312c02da89e4
Dadadon <davianberoni@yahoo.com>
2022-07-18 04:26:11 -0400

    update script file

--- script.js
+++ script.js
@@ -7,16 +7,19 @@ const links = document.querySelectorAll('.menu-link');
 /* ---Triggers the menu to open-- */
 openbtn.addEventListener('click', () => {
   menu.style.height = '100%';
+  document.body.style.overflow = "hidden";
 });
 
 /* ----Trigers the menu to close----- */
 
 closebtn.addEventListener('click', () => {
   menu.style.height = '0';
+  document.body.style.overflow = "visible";
 });
 
 links.forEach((link) => {
   link.addEventListener('click', () => {
     menu.style.height = '0';
+    document.body.style.overflow = "visible";
   });
 });
