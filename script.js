2acd1d3c2ae33c5fb0a09e706a65fa4e2c9994a2
Loltolo-Lesapiti <petrolesapiti@gmail.com>
2022-07-20 12:59:34 +0300

    Update the modal code

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
-/* ---Declaring variables */
+
 const openbtn = document.querySelector('.open');
 const closebtn = document.querySelector('.close');
 const menu = document.querySelector('.curtain');
 const links = document.querySelectorAll('.menu-link');
-
-/* ---Triggers the menu to open-- */
 openbtn.addEventListener('click', () => {
   menu.style.height = '100%';
   document.body.style.overflow = 'hidden';
 });
-
-/* ----Trigers the menu to close----- */
-
 closebtn.addEventListener('click', () => {
   menu.style.height = '0';
   document.body.style.overflow = 'visible';
@@ -23,10 +18,6 @@ links.forEach((link) => {
     document.body.style.overflow = 'visible';
   });
 });
-/** ***************************** *
-Code for Dinamically displaying the cards.
-********************************** */
-
 const cards = [
   {
     class: 'flex',
@@ -34,7 +25,6 @@ const cards = [
     heading3: 'CANOPY',
     list1: ['Back End Dev', '2015'],
     paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    mobileImage: 'images/card1-image.png',
     desktopImage: 'images/card1.png',
     list2: ['html', 'css', 'javaScript'],
     cardButton: 'See Project',
@@ -47,7 +37,6 @@ const cards = [
     heading3: 'FACEBOOK',
     list1: ['Full Stack Dev', '2015'],
     paragraph: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
-    mobileImage: 'images/card2-image.png',
     desktopImage: 'images/card2.png',
     list2: ['html', 'css', 'javaScript'],
     cardButton: 'See Project',
@@ -60,7 +49,6 @@ const cards = [
     heading3: 'FACEBOOK',
     list1: ['Full Stack Dev', '2015'],
     paragraph: 'Exploring the future of media in Facebooks first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.',
-    mobileImage: 'images/card3-image.png',
     desktopImage: 'images/card3.png',
     list2: ['html', 'css', 'javaScript'],
     cardButton: 'See Project',
@@ -73,7 +61,6 @@ const cards = [
     heading3: 'Uber',
     list1: ['Full Stack Dev', '2015'],
     paragraph: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
-    mobileImage: 'images/card4-image.png',
     desktopImage: 'images/card4.png',
     list2: ['html', 'css', 'javaScript'],
     cardButton: 'See Project',
@@ -81,8 +68,6 @@ const cards = [
     cardButton2: 'https://github.com/Loltolo-Lesapiti/loltolo-lesapiti.github.io',
   },
 ];
-
-// Creating a card div
 cards.forEach((card) => {
   const cardDiv = document.createElement('div');
   cardDiv.className = card.class;
@@ -93,24 +78,20 @@ cards.forEach((card) => {
   const ContentDiv = document.createElement('div');
   ContentDiv.className = 'flex_content';
   cardDiv.appendChild(ContentDiv);
-  // The title header
   const heading2 = document.createElement('h2');
   ContentDiv.appendChild(heading2);
   heading2.textContent = card.header2;
-
   const canopyDiv = document.createElement('div');
   canopyDiv.className = 'canopy formodal';
   ContentDiv.appendChild(canopyDiv);
-  // Images for mobile and desktop
   const mobileImage = document.createElement('img');
   mobileImage.className = 'mobile_image';
-  mobileImage.src = card.mobileImage;
+  mobileImage.src = card.desktopImage;
   imageDiv.appendChild(mobileImage);
   const DesktopImage = document.createElement('img');
   DesktopImage.className = 'desktop_image';
   DesktopImage.src = card.desktopImage;
   imageDiv.appendChild(DesktopImage);
-
   const heading3 = document.createElement('h3');
   canopyDiv.appendChild(heading3);
   heading3.textContent = card.heading3;
@@ -118,8 +99,6 @@ cards.forEach((card) => {
   paragraph.className = 'card-info';
   ContentDiv.appendChild(paragraph);
   paragraph.textContent = card.paragraph;
-
-  // Creating lists and list items.
   const list1 = document.createElement('ul');
   list1.className = 'canopy-list';
   canopyDiv.appendChild(list1);
@@ -128,8 +107,6 @@ cards.forEach((card) => {
     list1Elements.textContent = card.list1[j];
     list1.appendChild(list1Elements);
   }
-
-  // List two.
   const list2 = document.createElement('ul');
   list2.className = 'list second';
   ContentDiv.appendChild(list2);
@@ -138,18 +115,11 @@ cards.forEach((card) => {
     list2Elements.textContent = card.list2[j];
     list2.appendChild(list2Elements);
   }
-
-  // Creating a button
   const cardButton = document.createElement('button');
   cardButton.className = 'see-more displayModal';
   cardButton.textContent = card.cardButton;
   ContentDiv.appendChild(cardButton);
 });
-
-/**
- * Code for creating PopUp content
- */
-
 cards.forEach((card) => {
   const modal = document.createElement('div');
   modal.className = 'modal';
@@ -161,30 +131,24 @@ cards.forEach((card) => {
   closebtn.innerHTML = '&times;';
   content.appendChild(closebtn);
   closebtn.className = 'closeModal';
-
-  // The title header
   const heading2 = document.createElement('h2');
   content.appendChild(heading2);
   heading2.textContent = card.header2;
   heading2.className = 'h2formodal';
-
   const canopyDiv = document.createElement('div');
   canopyDiv.className = 'canopy canopymodal';
   content.appendChild(canopyDiv);
-
-  // Images for mobile and desktop
   const imageDiv = document.createElement('div');
-  imageDiv.className = 'flex_image';
+  imageDiv.className = 'flex_image enlarge';
   content.appendChild(imageDiv);
   const mobileImage = document.createElement('img');
-  mobileImage.className = 'mobile_image popImage';
-  mobileImage.src = card.mobileImage;
+  mobileImage.className = 'mobile_image';
+  mobileImage.src = card.desktopImage;
   imageDiv.appendChild(mobileImage);
   const DesktopImage = document.createElement('img');
-  DesktopImage.className = 'desktop_image';
+  DesktopImage.className = 'desktop_image  enl';
   DesktopImage.src = card.desktopImage;
   imageDiv.appendChild(DesktopImage);
-  // Pagraph and the buttton
   const ContentDiv = document.createElement('div');
   ContentDiv.className = 'flex_content pop';
   content.appendChild(ContentDiv);
@@ -196,8 +160,6 @@ cards.forEach((card) => {
   paragraph.className = 'card-info popParagrah';
   ContentDiv.appendChild(paragraph);
   paragraph.textContent = card.paragraph;
-
-  // Creating lists and list items.
   const list1 = document.createElement('ul');
   list1.className = 'canopy-list ul1formodal';
   canopyDiv.appendChild(list1);
@@ -206,7 +168,6 @@ cards.forEach((card) => {
     list1Elements.textContent = card.list1[j];
     list1.appendChild(list1Elements);
   }
-  // List two.
   const list2BTNDiv = document.createElement('div');
   list2BTNDiv.className = 'list2Btn';
   ContentDiv.appendChild(list2BTNDiv);
@@ -218,31 +179,25 @@ cards.forEach((card) => {
     list2Elements.textContent = card.list2[j];
     list2.appendChild(list2Elements);
   }
-
   const btnDiv = document.createElement('div');
   btnDiv.className = 'btnDiv';
   list2BTNDiv.appendChild(btnDiv);
-  // Creating a button
   const liveLink = document.createElement('a');
   liveLink.href = card.cardButton1;
   const cardButton1 = document.createElement('button');
   cardButton1.className = 'see-more';
   liveLink.appendChild(cardButton1);
   cardButton1.textContent = 'See Live';
-  btnDiv.appendChild(cardButton1);
+  btnDiv.appendChild(liveLink);
 
   const source = document.createElement('a');
   source.href = card.cardButton2;
   const cardButton2 = document.createElement('button');
   cardButton2.className = 'see-more';
   source.appendChild(cardButton2);
-  cardButton2.textContent = 'See Source';
-  btnDiv.appendChild(cardButton2);
+  cardButton2.innerHTML = 'See Source';
+  btnDiv.appendChild(source);
 });
-
-/**
-    * Javascript to Display the modal
-    */
 const buttons = document.querySelectorAll('.displayModal');
 const detailsWindow = document.querySelector('.modal');
 const closeModal = document.querySelector('.closeModal');
